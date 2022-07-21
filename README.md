bf8591e6012728ae715a340e975478e7a85a38b0
NEsTHOR <nesky66@gmail.com>
2022-07-21 18:12:09 -0300

    Cambio de Base  de Datos a MySQL

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PROYECTOFINAL_2022_Grupo4
+
+## Tema:
+> Integrantes<sub>nombre</sub>:
+
+
+
+
+
+
--- pryectoInfo2022/pryectoInfo2022/settings/local.py
+++ pryectoInfo2022/pryectoInfo2022/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'proyectoinfo',
+        'USER': 'root',
+        'PASSWORD': 'sys64738',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
