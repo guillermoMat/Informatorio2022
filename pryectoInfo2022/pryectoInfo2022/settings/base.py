d28f229f0b1249c2becb3a1bc490035d93d13926
Nestor Acevedo <nesky66@hotmail.com>
2022-07-25 14:56:51 -0300

    actualizacion user

--- pryectoInfo2022/pryectoInfo2022/settings/base.py
+++ pryectoInfo2022/pryectoInfo2022/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.usuarios.apps.UsuariosConfig',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
