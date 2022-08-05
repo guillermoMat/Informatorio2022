77fb65a64548fefbfd43fb34c4f4fd6b4f96f2b9
NEsTHOR <nesky66@gmail.com>
2022-08-05 10:21:10 -0300

    bis

--- pryectoInfo2022/apps/post/models.py
+++ pryectoInfo2022/apps/post/models.py
@@ -2,13 +2,14 @@ from datetime import time
 from email.headerregistry import ContentDispositionHeader
 from tkinter import Image, image_names
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
 class Post(models.Model):
-    titulo = models.CharField(max_length=30, blank=True, null=True),
-    descripcion = models.CharField(max_length=50, blank=True, null=True),
-    Contenido = models.CharField(max_length=400, blank=True, null=True),
-    # imagen = 
+    titulo = models.CharField(max_length=30, blank=True, null=False)
+    descripcion = models.CharField(max_length=50, blank=True, null=False)
+    contenido = models.CharField(max_length=400, blank=True, null=False)
+    # imagen =
     # fech_creacion = models.TimeField(_("fecha creacion"), auto_now=False, auto_now_add=False),
     
     # estado
