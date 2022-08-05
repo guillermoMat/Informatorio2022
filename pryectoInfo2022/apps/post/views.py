dc8ec850282f5ee6c43f40ec8c454dd601326e81
NEsTHOR <nesky66@gmail.com>
2022-08-05 00:06:47 -0300

    creacion modelo ushtml5 uario + migrate

--- pryectoInfo2022/apps/post/views.py
+++ pryectoInfo2022/apps/post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    nombre = "Nestor Acevedo"
+    lista = ["Acevedo","Alegre","Alvarin","Alvarez","Busi","Echavarria","Mathieu","Salcedo","Sotelo"]
+    return render(request,'inicio.html', {"nom":nombre,"lis":lista})
+    
+   
+
+    
--- pryectoInfo2022/pryectoInfo2022/views.py
+++ pryectoInfo2022/pryectoInfo2022/views.py
@@ -1,12 +1,4 @@
-from http.client import HTTPResponse
+# from http.client import HTTPResponse
 from django.shortcuts import render
 
 
-def inicio(request):
-    nombre = "Nestor Acevedo"
-    lista = ["Acevedo","Alegre","Alvarin","Alvarez","Busi","Echavarria","Mathieu","Salcedo","Sotelo"]
-    return render(request,'inicio.html', {"nom":nombre,"lis":lista})
-    
-   
-
-    
