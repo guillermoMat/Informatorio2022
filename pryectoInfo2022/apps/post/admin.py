35b48c738566587473ab11e7beee3013571bd9f3
Nestor Acevedo <nesky66@hotmail.com>
2022-08-05 19:29:47 -0300

    ...

--- pryectoInfo2022/apps/post/admin.py
+++ pryectoInfo2022/apps/post/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-
+from apps.usuarios.models import Usuario
 # Register your models here.
 from apps.post.models import Categoria, Post
 
 class AdminPost(admin.ModelAdmin):
-    list_display =   ('titulo','descripcion','contenido')
+    list_display =   ('id','titulo','descripcion','contenido','fecha_creacion',
+                      'estado')
+    
+
+class AdminCategoria(admin.ModelAdmin):
+    list_display =   ('id','categoria')
+
 
 admin.site.register(Post,AdminPost)
+admin.site.register(Categoria,AdminCategoria)
--- pryectoInfo2022/apps/post/models.py
+++ pryectoInfo2022/apps/post/models.py
@@ -1,20 +1,28 @@
 from datetime import time
 from django.db import models
-from apps.usuarios.models import Usuario
+# from apps.usuarios.models import Usuario
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
 
 class Categoria(models.Model):
     categoria = models.CharField(max_length=15, blank=False,null=True)
+    
+    def __str__(self):
+        return self.categoria
 
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=30, blank=False, null=True)
-    descripcion = models.CharField(max_length=50, blank=False, null=True)
-    contenido = models.CharField(max_length=400, blank=False, null=True)
+    titulo = models.CharField(max_length=200, blank=False, null=True)
+    descripcion = models.CharField(max_length=200, blank=False, null=True)
+    contenido = models.TextField(null=True)
     # imagen =
-    fecha_creacion = models.TimeField(auto_now=False, auto_now_add=False),
+    fecha_creacion = models.DateTimeField(default=time),
     estado = models.CharField
     categoria_id = models.ForeignKey(Categoria, on_delete=models.CASCADE,null=True)
-    usuario_id = models.ForeignKey(Usuario, on_delete=models.CASCADE,null=True)
+    usuario_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    
+    def __str__(self):
+        return self.titulo
     
