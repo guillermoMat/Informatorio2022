9b5722259f9e8b16e9085489725d5aea828a72fe
NEsTHOR <nesky66@gmail.com>
2022-08-23 02:17:37 -0300

    actualizacion auth

--- pryectoInfo2022/apps/usuarios/views.py
+++ pryectoInfo2022/apps/usuarios/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-class VRegistro(View):
+
+class VRegistro(LoginRequiredMixin, View):
     
     def get(self, request):
+        
         form=UserCreationForm()
         return render(request,'usuarios/registro.html',{'form':form})
     
